6d831c3b537a3b1d5cc1398e3051947680a86b2b
aryamccarthy <arya.mccarthy@gmail.com>
2017-10-06 14:34:44 -0400

    Implement basic suffix tree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from suffixtree import SuffixTree
+from predictiontree import PredictionSuffixTree as PST
 
 test_strings = ["abracadabra", "abcccdaadcdaabcad"]
 L = 3
@@ -7,4 +7,12 @@ r = 2
 
 
 if __name__ == '__main__':
-    tree = SuffixTree()
+    tree = PST('there would have been a time for such a word')
+    print(tree.has_substring("nope"))
+    print(tree.has_substring("would have been"))
+    print(tree.has_suffix("such a word"))
+
+    # tree.search(test_strings[0], L)
+    # print("Tree structure before elimination:")
+    # tree.list()
+    # tree.eliminate_empirical(test_strings[0], p_min)
---
+++ predictiontree.py
@@ -0,0 +1,7 @@
+from suffixtree import SuffixTree
+
+class PredictionSuffixTree(SuffixTree):
+    """docstring for PredictionSuffixTree"""
+    def __init__(self, s):
+        super(PredictionSuffixTree, self).__init__(s)
+        
